4394534047df5cdfcd58b98da8fb9c9c20eea288
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-02-20 20:17:15 -0500

    Modules complete

---
+++ test_security_contact_wiring/build/test_security_contact_wiring.cpp
@@ -0,0 +1,76 @@
+//Button pin
+#include "WProgram.h"
+void setup();
+void loop();
+void setColor(int redValue, int greenValue, int blueValue);
+const int CONTACT_PIN = 1;
+
+//R LED pin
+const int R_LED_PIN = 10;
+
+//G LED pin
+const int G_LED_PIN = 12;
+
+//B LED pin
+const int B_LED_PIN = 13;
+
+//Door state
+boolean open;
+
+//Attribute that defines the button state
+boolean buttonState;
+
+//Current time when the button is tapped
+long currTime;
+
+//Number of current attempts
+byte attempts;
+
+void setup()
+{
+  Serial.begin(9600);
+  buttonState = false;
+  
+  pinMode(R_LED_PIN, OUTPUT);
+  pinMode(G_LED_PIN, OUTPUT);
+  pinMode(B_LED_PIN, OUTPUT);
+  pinMode(CONTACT_PIN,INPUT);
+  
+  setColor(0, 255, 255);
+}
+
+void loop()
+{
+   //Button input read and processing 
+  if(!buttonState) {
+    Serial.println(digitalRead(CONTACT_PIN));
+    if(digitalRead(CONTACT_PIN)) {
+      currTime = millis();
+      buttonState = true;
+      Serial.println("Contact on!");
+    }
+  }
+  else {
+    if(!digitalRead(CONTACT_PIN)) {
+      if((millis()-currTime)>=5000) {
+        setColor(0, 255, 0);
+        open = true;
+        Serial.println("Door opened!!");
+        attempts = 0;
+      }
+      else {
+        Serial.println("NOT ENTOUGH TIME");
+      }
+      buttonState = false;
+    }
+  }
+  delay(100);
+}
+
+//Method that outputs the RGB specified color
+void setColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(R_LED_PIN, redValue);
+  analogWrite(G_LED_PIN, greenValue);
+  analogWrite(B_LED_PIN, blueValue);
+}
+
--- test_securtity_pir_arduino/test_securtity_pir_arduino.ino
+++ test_securtity_pir_arduino/test_securtity_pir_arduino.ino
@@ -1,118 +1,36 @@
-#include <Keypad.h>
-
-//Specified password
-const String KEY = "1234";
-
-//Keypad rows
-const byte ROWS = 4; 
-
-//Keypad columns
-const byte COLS = 3;
-
-//Maximum number of attempts allowed
-const byte maxAttempts = 3;
-
-//Keypad mapping matrix
-char hexaKeys[ROWS][COLS] = {
-  {
-    '1', '2', '3'
-  }
-  ,
-  {
-    '4', '5', '6'
-  }
-  ,
-  {
-    '7', '8', '9'
-  }
-  ,
-  {
-    '*', '0', '#'
-  }
-};
-
-//Keypad row pins definition
-byte rowPins[ROWS] = {
-  9, 8, 7, 6
-}; 
-
-//Keypad column pins definition
-byte colPins[COLS] = {
-  5, 4, 3
-};
-
-//Keypad library initialization
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
-
-//Current key variable
-String currentKey;
-//Door state
-boolean open;
-//Number of current attempts
-byte attempts;
-//If the number of current attempts exceeds the maximum allowed
-boolean block;
-
+/*
+ * PIR sensor tester
+ */
+ 
+int ledPin = 11;                // choose the pin for the LED
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+ 
 void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+ 
   Serial.begin(9600);
-  currentKey = "";
-  open = false;
-  attempts = 0;
-  block = false;
 }
-
-void loop() {
-
-  char customKey;
-
-  if(!block) {
-    //Selected key parsed;
-    customKey = customKeypad.getKey();
-  }
-  else {
-    Serial.println("Number of attempts exceeded");
-    while(true);
-  }
-
-  //Verification of input and appended value
-  if (customKey) {  
-    currentKey+=String(customKey);
-    Serial.println(currentKey);
-  }
-
-  //If the current key contains '*' and door is open
-  if(open && currentKey.endsWith("*")) {
-    open = false;
-    Serial.println("Door closed");
-    digitalWrite(10,LOW);
-    currentKey = "";
-  }
-  //If the current key contains '#' reset attempt
-  if(currentKey.endsWith("#")&&currentKey.length()<=KEY.length()) {
-    currentKey = "";
-    Serial.println("Attempt deleted");
-  }
-
-  //If current key matches the key length
-  if (currentKey.length()== KEY.length()) {
-    if(currentKey == KEY) {
-      digitalWrite(10,HIGH);
-      open = true;
-      Serial.println("Door opened!!");
-      attempts = 0;
+ 
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
     }
-    else {
-      attempts++;
-      currentKey = "";
-      Serial.println("Number of attempts: "+String(attempts));
+  } else {
+    digitalWrite(ledPin, LOW); // turn LED OFF
+    if (pirState == HIGH){
+      // we have just turned of
+      Serial.println("Motion ended!");
+      // We only want to print on the output change, not state
+      pirState = LOW;
     }
-  }else if(currentKey.length()> KEY.length()){
-    Serial.println("Door opened!!");
   }
-  if(attempts>=maxAttempts) {
-    block = true;
-  }
-
-  delay(100);
 }
-
