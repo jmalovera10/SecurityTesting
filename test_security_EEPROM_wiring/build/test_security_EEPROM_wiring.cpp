db7c0a6949f4b63d649da6128d05cd286bdf20f0
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-03-29 19:39:20 -0500

    EEPROM example

---
+++ test_security_EEPROM_wiring/build/test_security_EEPROM_wiring.cpp
@@ -0,0 +1,24 @@
+#include <EEPROM.h>
+
+#include "WProgram.h"
+void setup();
+void loop();
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println(EEPROM.read(0));
+  Serial.println(EEPROM.read(1));
+  delay(random(5000));
+  long time = millis();
+  int addr0 = time%256;
+  int addr1 = (time/256) % 256;
+  Serial.println(String(addr0)+" & "+String(addr1));
+  EEPROM.write(0,addr0);
+  EEPROM.write(1,addr1);
+}
+
+void loop()
+{
+
+}
+
